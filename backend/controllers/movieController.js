134de02cf25e1f7868f970925dadaaa0ede684d8
Tiago Gomes <tidgomes@gmail.com>
2023-11-09 16:51:14 -0300

    refactor: change folder structure

--- backend/controllers/movieController.js
+++
@@ -1,3 +0,0 @@
-export const getAllMovies = (req, res) => {
-  res.status(201).json({ message: 'Get all movies' })
-}
--- backend/routes/movieRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getAllMovies } from '../controllers/movieController.js'
-
-const router = express.Router()
-
-router.get('/', getAllMovies)
-
-export default router
