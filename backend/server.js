10a87d026d5603bb3631042b0b47a2188d5b86c1
Tiago Gomes <tidgomes@gmail.com>
2023-11-10 12:19:18 -0300

    feat: create middlewares to handle errors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import movieRouter from './routes/movieRoutes.js'
 import { connectDB } from './config/db.js'
+import errorHandler, { notFound } from './middleware/errorHandler.js'
 
 dotenv.config()
 connectDB()
@@ -13,6 +14,9 @@ server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
 server.use('/api/movies', movieRouter)
+
+server.use(notFound)
+server.use(errorHandler)
 
 server.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`)
