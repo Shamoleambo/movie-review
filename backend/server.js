8a46f71166c50f3499d540a058d43d9a12975e01
Tiago Gomes <tidgomes@gmail.com>
2023-11-10 11:29:33 -0300

    feat: parse incoming requests for json and urlencoded

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import movieRouter from './routes/movieRoutes.js'
 
 const server = express()
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
 
 server.use('/api/movies', movieRouter)
 
