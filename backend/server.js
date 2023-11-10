f3ca2ceec4949d5049d7db2baad9d53fbc2004f7
Tiago Gomes <tidgomes@gmail.com>
2023-11-10 11:37:09 -0300

    feat: configure port on .env

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ import movieRouter from './routes/movieRoutes.js'
 dotenv.config()
 const server = express()
 
+const PORT = process.env.PORT || 5000
+
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
 server.use('/api/movies', movieRouter)
 
-server.listen(3000, () => {
-  console.log('hello express')
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`)
 })
