9ea66689c96ad20024d3b7e872bda58dd832d31d
Tiago Gomes <tidgomes@gmail.com>
2023-11-10 11:29:07 -0300

    feat: create route to register a movie

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getMovies } from '../controllers/movieController.js'
+import { getMovies, registerMovie } from '../controllers/movieController.js'
 
 const router = express.Router()
 
 router.get('', getMovies)
+router.post('/register', registerMovie)
 
 export default router
