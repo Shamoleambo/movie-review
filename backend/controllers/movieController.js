2b95eaaa931487f4e41723630a3124062d56e966
Tiago Gomes <tidgomes@gmail.com>
2023-11-10 11:28:43 -0300

    feat: create simple controller to register a new movie

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -3,3 +3,8 @@ import asyncHandler from 'express-async-handler'
 export const getMovies = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'hello getMovies' })
 })
+
+export const registerMovie = asyncHandler(async (req, res) => {
+  const { name, year } = req.body
+  res.status(201).json({ name, year })
+})
