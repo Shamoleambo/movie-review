6d6fdccbf83855e6fa4e0ddab56845aaf6d3ade4
Tiago Gomes <tidgomes@gmail.com>
2023-11-14 08:19:01 -0300

    feat: create user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/user.js'
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, passwordConfirmation } = req.body
@@ -7,4 +8,12 @@ export const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Required param is missing')
   }
+
+  const response = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirmation
+  })
+  res.status(201).json(response)
 })
