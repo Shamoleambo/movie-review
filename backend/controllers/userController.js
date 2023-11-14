ea6c90f6add2e087d171db8c83d9c1a479ede32c
Tiago Gomes <tidgomes@gmail.com>
2023-11-14 08:13:28 -0300

    feat: throw error in response in case required user params are missing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 import asyncHandler from 'express-async-handler'
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Register user' })
+  const { name, email, password, passwordConfirmation } = req.body
+
+  if (!name || !email || !password || !passwordConfirmation) {
+    res.status(400)
+    throw new Error('Required param is missing')
+  }
 })
