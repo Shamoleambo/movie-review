0556e12a6688bc71cf7b45eb724012cd791411bf
Tiago Gomes <tidgomes@gmail.com>
2023-11-14 08:07:33 -0300

    feat: make simple register user controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import movieRouter from './routes/movieRoutes.js'
+import userRouter from './routes/userRouter.js'
 import { connectDB } from './config/db.js'
 import errorHandler, { notFound } from './middleware/errorHandler.js'
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
+server.use('/api/users', userRouter)
 server.use('/api/movies', movieRouter)
 
 server.use(notFound)
